fd42ee12bc0d753872adb5de59da466181a4901d
mucahiddayan <muecahid@dayan.one>
2017-09-14 10:01:56 +0200

    just readme

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      var video  = document.querySelector('video');
-      chrome.extension.getBackgroundPage().console.log(request.time);
-      if(request.func == 'saved'){
-        chrome.extension.getBackgroundPage().console.log(request);
-      }
-    });
